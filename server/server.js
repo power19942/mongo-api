985ef84ef060acab2d2625a583d45e2663617cd8
omar jakmira <poweromar19942@gmail.com>
2017-05-05 17:39:16 -0800

    add post /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var {mongoose} = require("./DB/mongoose.js");
+var {Todo} = require("./models/todo.js");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+    var todo = new Todo({
+        text:req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000,()=>{
+   console.log("run in 3000") ;
+});
