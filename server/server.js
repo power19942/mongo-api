ec87f84144068713c1182310a38983b6cb41486d
omar jakmira <poweromar19942@gmail.com>
2017-05-06 11:04:21 -0800

    final commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var {ObjectID} = require("mongodb");
+
 var {mongoose} = require("./DB/mongoose.js");
 var {Todo} = require("./models/todo.js");
 
 var app = express();
+var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -27,6 +30,22 @@ app.get('/todos',(req,res)=>{
    }) ;
 });
 
-app.listen(3000,()=>{
-   console.log("run in 3000") ;
+app.get("/todos/:id",(req,res)=>{
+    var id = req.param('id');
+    if(ObjectID.isValid(id)){
+       Todo.findById(id).then((todo)=>{
+           if(!todo){
+               return res.status(404).send();
+           }
+           res.send({todo});
+       }).catch((e)=>{
+           return res.status(400).send();
+       });
+    }else{
+        return res.status(404).send();
+    }
+});
+
+app.listen(port,()=>{
+   console.log("run in "+port) ;
 });
